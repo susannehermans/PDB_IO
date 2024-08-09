c9e04047a6b8cbee0fb09e2f060dbfa7f8851624
susannehermans <susanne.hermans@gmail.com>
2024-08-09 15:51:33 +0200

    Update PDBLine.py

--- src/PDBLine.py
+++ src/PDBLine.py
@@ -1,9 +1,9 @@
 class PDBLine:
-	def __init__(self, line):
-		self.line = line
+    def __init__(self, line):
+        self.line = line
 
-	def __str__(self) -> str:
-		return self.line
+    def __str__(self) -> str:
+        return self.line
     
     #@classmethod
     def from_line(cls, line):
